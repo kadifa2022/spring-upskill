dbe1281ccbab92d4ca008d87342de63d34e53a5a
Kadifa <Kudickadifa@gmail.com>
2023-11-01 19:25:41 -0500

    Initial commit

---
+++ src/main/java/com/cydeo/SpringUpskillApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUpskillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUpskillApplication.class, args);
+    }
+
+}
